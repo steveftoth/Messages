dc457a604c9a557675362630ac70c5dbc7affdd9
Steven Toth <steve@toths.info>
2015-07-10 06:57:31 -0700

    updating paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,29 +4,39 @@ module.exports = function(grunt) {
       build: [ 'dist' ] 
     },
     bower: {
-      dev: { 
-        dest: 'dist/deps',
-        js_dest: 'dist/
+      dev: {
+        dest: 'dist/web/'
       }
     },
     less: {
       development: {
         options: {
-          paths: [ 'web/src', 'dist/deps/less' ]
+          paths: [ 'dist/web/css','bower_components/bootstrap/less' ]
         },
         files: {
-           'dist/web/messages/messages.css' : 'web/src/css/messages.less'
+           'dist/web/css/messages.css' : 'dist/web/css/messages.less',
+           'dist/web/css/bootstrap.css' : 'dist/web/less/bootstrap.less'
         }
-     }
-   },
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'web/src/' ,src: ['js/**'], dest: 'dist/web/'},
+          {expand: true, cwd: 'web/src/' ,src: ['css/**'], dest: 'dist/web/'},
+          {expand: true, cwd: 'web/src/html' ,src: ['**'], dest: 'dist/web/'}
+        ]
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');  
   grunt.loadNpmTasks('grunt-bower');
   grunt.loadNpmTasks('grunt-contrib-less');  
   grunt.loadNpmTasks('grunt-bowercopy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['clean','bower','less']);
+  grunt.registerTask('default', ['clean','copy','bower','less']);
 };
 
 
